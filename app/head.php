3c9965a8ed2b1b0a71c67c061cd5cb8035a233ff
mikeo26 <mikeo26@gmail.com>
2016-04-20 11:06:17 +0200

    Aanpassingen

--- app/head.php
+++ app/head.php
@@ -7,29 +7,24 @@
 	<script src="assets/js/queries.js"></script>
 
 	<!-- Optional theme -->
+	<link rel="stylesheet" href="assets/css/style.css">
 	<link rel="stylesheet" href="https://bootswatch.com/cerulean/bootstrap.min.css">
 </head>
 
 <body>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				
-			</div>
-			<div class="col-sm-2">
-				<p class="info-mail">info@mdwa.nl</p>
-			</div>
 
-			<div class="col-md-6">
-				<a href="#" class="pull-right">test</a>
-				<a href="#" class="pull-right">test</a>
-			</div>
-		</div>
-	</div>
 
 
 			<img style="width: 100%;" src="assets/img/site-banner.jpg" alt="site-banner.jpg">
+				<div class="container">
+		<div class="top-head row">
+			<div class="col-sm-12">
+				<p class="info-mail text-center"><span>info@mdwa.nl</span> <a href="#" class="pull-right">Inloggen</a> <a href="#" class="pull-right" style="margin-right:5px;">Registreren</a> </p>
+
+				</div>
+		</div>
+	</div>
 
 
 	<nav class="navbar navbar-default" style="border-radius: 0">
@@ -42,35 +37,32 @@
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </button>
-	      <a class="navbar-brand" href="#">Brand</a>
+	      <a class="navbar-brand" href="#">MDWA</a>
 	    </div>
 
 	    <div class="navbar-collapse collapse" id="bs-example-navbar-collapse-1" aria-expanded="false" style="height: 1px;">
 	      <ul class="nav navbar-nav">
-	        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-	        <li><a href="#">Link</a></li>
-	        <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+	        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+	        <li><a href="/films">Films</a></li>
+	        <li><a href="/zoeken">Zoeken</a></li>
+			<li class="dropdown">
+	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mijn account <span class="caret"></span></a>
 	          <ul class="dropdown-menu" role="menu">
-	            <li><a href="#">Action</a></li>
-	            <li><a href="#">Another action</a></li>
-	            <li><a href="#">Something else here</a></li>
+	            <li><a href="/mijnaccount">Mijn account</a></li>
+	            <li><a href="/registreren">Registreren</a></li>
+	            <li><a href="/inloggen">Inloggen</a></li>
 	            <li class="divider"></li>
-	            <li><a href="#">Separated link</a></li>
-	            <li class="divider"></li>
-	            <li><a href="#">One more separated link</a></li>
+	            <li><a href="#">Info</a></li>
 	          </ul>
 	        </li>
+	        <li><a href="/contact">Contact</a></li>
 	      </ul>
-	      <form class="navbar-form navbar-left" role="search">
+	      <form class="navbar-form navbar-right" role="search">
 	        <div class="form-group">
 	          <input type="text" class="form-control" placeholder="Search">
 	        </div>
 	        <button type="submit" class="btn btn-default">Submit</button>
 	      </form>
-	      <ul class="nav navbar-nav navbar-right">
-	        <li><a href="#">Link</a></li>
-	      </ul>
 	    </div>
 	    </div>
 	  </div>
