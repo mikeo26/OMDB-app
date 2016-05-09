25f1c52a61289f679d5384b48310cd92b4abf127
Menno Laptop <menno.vd.gmail.com>
2016-05-09 10:45:38 +0200

    Kleine aanpassingen (naam, datum, klas etc in footer)

--- app/assets/js/queries.js
+++ app/assets/js/queries.js
@@ -9,12 +9,10 @@ $.ajax({
   success: function(data){
 
     for(i=0; i < 7; i++){
-            $('#aanbevolenFilmContent').append("(" + data[i].year + ") " + data[i].title + "<br />");
+            
+            $('#aanbevolenFilmContent').append("<div id='movie' movie-id='"+data[i].id+"'><span>("+data[i].year+") "+data[i].title+"</span>");
             $('.loading').hide();
             console.log(data[i].title);
-
-            // var movieId = data[i].id;
-            // $('aanbevolenFilmPoster').append(data[i].posters.small);
         }
     },
     error: function(){
@@ -23,3 +21,20 @@ $.ajax({
     },
     timeout: 5000 // set timeout to 5 seconds
 });
+
+setTimeout( function() {
+  $("#movie").each( function() {
+  var id = $(this).attr("movie-id");
+  console.log("derp");
+    $.ajax({
+    dataType: "jsonp",
+    url: 'http://www.moviemeter.nl/api/film/'+id+'&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
+    type: "GET",
+    cache: false,
+    async: true,
+    success: function(data) {
+      $("#movie").append("<img src="+data.posters.small+" />");
+    }
+  })
+});
+}, 300);
