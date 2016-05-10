722adc0f2bfd427c6176428f50f4dda960b84cee
Menno Laptop <menno.vd.gmail.com>
2016-05-10 13:59:07 +0200

    Footer link Menno website

--- app/footer.php
+++ app/footer.php
@@ -2,7 +2,7 @@
 		<div class="row">
 			<div class="col-md-12">
 				<br><br>
-				<p>&copy; 2016 | Menno van der Krift en Mike Oerlemans</p>
+				<p>&copy; 2016 | <a href="http://www.mennovanderkrift.com" target="_blank"> Menno van der Krift</a> en Mike Oerlemans</p>
 			</div>
 		</div>
 	</div>
