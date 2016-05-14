38da680f3109a318f0bd4b55f36d423787360813
Menno van der Krift (desktop) <menno.vd.krift@gmail.com>
2016-05-14 21:58:46 +0200

    Enkele film en 404 pagina aangemaakt.

---
+++ app/404.php
@@ -0,0 +1,13 @@
+<?php include 'head.php'; ?>
+
+
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12">
+			<h1 class="page-title">404 - Pagina niet gevonden</h1>
+			<p> Oops! De pagina waar je naar zocht bestaat niet (meer) of is verplaatst.</p>
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
---
+++ app/film.php
@@ -0,0 +1,82 @@
+<?php include('head.php'); ?>
+
+<?php
+
+	// random movie-id: 104182
+
+	if(!isset($_GET['movie-id'])) { // als GET movie-id niet bestaat, ga naar 404 pagina
+		header("location: 404.php");
+	} else if (empty($_GET['movie-id'])) { // als GET movie-id leeg is, ga naar 404 pagina
+		header("location: 404.php");
+	} else if (is_int($_GET['movie-id'])){ // als GET movie-id geen nummer is, ga naar 404 pagina
+		header ("location: 404.php");
+	} else {
+		$movieId = $_GET['movie-id'];
+	}
+
+	// als film met movie-id == 0, direct naar 404 pagina
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 col-lg-12">
+			<span id="movie-id" style="display: none;"><?= $movieId ?></span>
+			<h1 class="movie-title"></h1>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-4 col-md-4 col-lg-4">
+ 			<img src="assets/img/movie-poster.jpg" alt="Movie poster" class="movie-poster img-rounded" style="width: 100%">
+ 		</div>
+
+		<div class="col-sm-8 col-md-8 col-lg-8">
+			<p class="movie-plot"><br></p>
+			<small class="movie-genres">Genres: </small><br>
+			<small class="movie-director">Regiseur: &nbsp;</small><br>
+			<small class="movie-actors">Acteurs: &nbsp;</small><br>
+			<small class="movie-duration">Speelduur: <span class="movie-time"></span> minuten</small><br>
+			<small class="movie-rating">Waardering: <span class="movie-score"></span>/5</small>
+		</div>
+	</div>
+</div>
+
+<script>
+var errormsg = '<p class="errormsg reload" onClick="window.location.reload()">De content kon niet worden geladen <br /> Klik hier om het nog een keer te proberen.</p>';
+var movieId = document.getElementById('movie-id').innerHTML;
+
+$.ajax({
+	dataType: "jsonp",
+	url: 'http://www.moviemeter.nl/api/film/' + movieId + '&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
+	type: "GET",
+	cache: false,
+	async: true,
+	success: function(data){          
+	  $('.movie-title').append(data.title + " <small>(" + data.year + ")</small>");
+
+	  for(i=0; i < data.genres.length; i++){
+	  	$('.movie-genres').append(data.genres[i]);
+	  	$('.movie-genres').append(', ');
+	  }
+
+	  $('.movie-plot').append(data.plot);
+	  $('.movie-director').append(data.directors);
+	  // $('.movie-actors').append(data.actors[0].name); //nog loopen door alle acteurs
+	  for(i=0; i < data.actors.length; i++){
+	  	$('.movie-actors').append(data.actors[i].name);
+	  	$('.movie-actors').append(', ');
+	  }
+
+	  $('.movie-time').append(data.duration);
+	  $('.movie-score').append(data.average);
+	  $('.loading').hide();
+	},
+	error: function(){
+	$('#aanbevolenFilmsContent').append(errormsg);
+	$('.loading').hide();
+	},
+	timeout: 5000 // set timeout to 5 seconds
+});
+
+</script>
+
+<?php include('footer.php'); ?>
