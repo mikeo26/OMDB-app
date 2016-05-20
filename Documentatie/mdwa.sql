ddb86d8217836d5725b0eae039f696da10525d74
Menno Laptop <menno.vd.gmail.com>
2016-05-20 12:09:33 +0200

    database aangepast

--- Documentatie/mdwa.sql
+++ Documentatie/mdwa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2016 at 11:58 AM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Generation Time: May 20, 2016 at 10:08 AM
+-- Server version: 5.7.9
+-- PHP Version: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `mdwa`
@@ -26,11 +26,19 @@ SET time_zone = "+00:00";
 -- Table structure for table `bekeken`
 --
 
+DROP TABLE IF EXISTS `bekeken`;
 CREATE TABLE IF NOT EXISTS `bekeken` (
-  `gebruikersId` int(11) NOT NULL,
-  `filmId` int(11) NOT NULL,
-  UNIQUE KEY `gebruikersId` (`gebruikersId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `gebruikersId` int(9) NOT NULL,
+  `filmId` int(9) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bekeken`
+--
+
+INSERT INTO `bekeken` (`gebruikersId`, `filmId`) VALUES
+(4, 83184),
+(4, 83184);
 
 -- --------------------------------------------------------
 
@@ -38,11 +46,11 @@ CREATE TABLE IF NOT EXISTS `bekeken` (
 -- Table structure for table `collectie`
 --
 
+DROP TABLE IF EXISTS `collectie`;
 CREATE TABLE IF NOT EXISTS `collectie` (
-  `gebruikersId` int(11) NOT NULL,
-  `filmId` int(11) NOT NULL,
-  UNIQUE KEY `gebruikersId` (`gebruikersId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `gebruikersId` int(9) NOT NULL,
+  `filmId` int(9) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -50,6 +58,7 @@ CREATE TABLE IF NOT EXISTS `collectie` (
 -- Table structure for table `gebruikers`
 --
 
+DROP TABLE IF EXISTS `gebruikers`;
 CREATE TABLE IF NOT EXISTS `gebruikers` (
   `gebruikersid` int(11) NOT NULL AUTO_INCREMENT,
   `gebruikersrol` varchar(1) NOT NULL,
@@ -58,15 +67,16 @@ CREATE TABLE IF NOT EXISTS `gebruikers` (
   `wachtwoord` text NOT NULL,
   `verificatieCode` text NOT NULL,
   PRIMARY KEY (`gebruikersid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `gebruikers`
 --
 
 INSERT INTO `gebruikers` (`gebruikersid`, `gebruikersrol`, `emailadres`, `gebruikersnaam`, `wachtwoord`, `verificatieCode`) VALUES
-(1, '1', 'menno.vd.krift@gmail.com', 'menno', 'admin', ''),
-(2, '1', 'mikeo26@gmail.com', 'mike', 'admin', '');
+(2, '1', 'mikeo26@gmail.com', 'mike', 'admin', ''),
+(3, '1', 'peter.vanderkrift@casema.nl', 'peter', 'admin', ''),
+(6, '0', 'menno.vd.krift@gmail.com', 'menno', '$2y$11$SKxWk6iuBGDZeyODZ5MiveLqltVkrBAdUHqYn3015sYT/I3EmUehe', 'gabd5pqsnoe86971mxlcikrjt40z23yhf');
 
 -- --------------------------------------------------------
 
@@ -74,11 +84,11 @@ INSERT INTO `gebruikers` (`gebruikersid`, `gebruikersrol`, `emailadres`, `gebrui
 -- Table structure for table `watchlist`
 --
 
+DROP TABLE IF EXISTS `watchlist`;
 CREATE TABLE IF NOT EXISTS `watchlist` (
-  `gebruikersId` int(11) NOT NULL,
-  `filmId` int(11) NOT NULL,
-  UNIQUE KEY `gebruikersId` (`gebruikersId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `gebruikersId` int(9) NOT NULL,
+  `filmId` int(9) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -86,39 +96,11 @@ CREATE TABLE IF NOT EXISTS `watchlist` (
 -- Table structure for table `wishlist`
 --
 
+DROP TABLE IF EXISTS `wishlist`;
 CREATE TABLE IF NOT EXISTS `wishlist` (
-  `gebruikersId` int(11) NOT NULL,
-  `filmId` int(11) NOT NULL,
-  UNIQUE KEY `gebruikersId` (`gebruikersId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `bekeken`
---
-ALTER TABLE `bekeken`
-  ADD CONSTRAINT `bekeken_ibfk_1` FOREIGN KEY (`gebruikersId`) REFERENCES `gebruikers` (`gebruikersid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `collectie`
---
-ALTER TABLE `collectie`
-  ADD CONSTRAINT `collectie_ibfk_1` FOREIGN KEY (`gebruikersId`) REFERENCES `gebruikers` (`gebruikersid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `watchlist`
---
-ALTER TABLE `watchlist`
-  ADD CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`gebruikersId`) REFERENCES `gebruikers` (`gebruikersid`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `wishlist`
---
-ALTER TABLE `wishlist`
-  ADD CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`gebruikersId`) REFERENCES `gebruikers` (`gebruikersid`) ON DELETE CASCADE ON UPDATE CASCADE;
+  `gebruikersId` int(9) NOT NULL,
+  `filmId` int(9) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
