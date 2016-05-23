962ca2943ee290bb3eca5d85ba1a926aaf32f012
Menno Laptop <menno.vd.gmail.com>
2016-05-23 13:27:47 +0200

    zoeken.php

--- app/zoeken.php
+++ app/zoeken.php
@@ -30,7 +30,7 @@ if(isset($_GET['zoekterm'])){
 <?php include('footer.php'); ?>
 
 <script>
-var errormsg = '<p class="errormsg reload" onClick="window.location.reload()">Geen zoekresultaten om weer te geven. Controleer op spel en typfouten en probeer het opnieuw.</p>';
+var errormsg = '<p class="errormsg reload col-sm-12 col-md-12 col-lg-12" onClick="window.location.reload()">Geen zoekresultaten om weer te geven. Controleer op spel en typfouten en probeer het opnieuw.</p>';
 
 var filmdata;
 
@@ -38,84 +38,67 @@ var zoekterm = document.getElementById('zoekterm').innerHTML;
 document.getElementById('zoekveld').setAttribute("value", zoekterm);
 
 
-  $.ajax({
-    dataType: "jsonp",
-    url: 'http://www.moviemeter.nl/api/film/?q=' + zoekterm + '&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
-    type: "GET",
-    cache: false,
-    async: true,
-    success: function(data){
-    	// Haal 5 films op
-    	for(i=0; i < 100; i++){
-        
-        	if (data[i].id !== undefined || data[i].id !== ''){
-	        	filmdata = {
-	        		id: data[i].id,
-	        		title: data[i].title,
-	        		year: data[i].year,
-	        		poster: ''
-	        	}
+$.ajax({
+dataType: "jsonp",
+url: 'http://www.moviemeter.nl/api/film/?q=' + zoekterm + '&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
+type: "GET",
+cache: false,
+async: true,
+success: function(data){
 
-	        	moviemeterCall(filmdata.id);
-			}
+	// Haal 50 films op
+	for(i=0; i < 50; i++){
+    
+    	if (data[i].id !== undefined || data[i].id !== ''){
+        	filmdata = {
+        		id: data[i].id,
+        		title: data[i].title,
+        		year: data[i].year,
+        		poster: ''
+        	}
 
-			
+        	moviemeterCall(filmdata.id, data[i]);
+		}
+	} // for loop end
+},
+error: function(){
+    $('.zoekresultaten').append(errormsg);
+    $('.loading').hide();
+  },
+  timeout: 5000 // set timeout to 5 seconds
+}); // ajax request end
 
-			
-      	} // for loop end
-    },
-    error: function(){
-        $('.zoekresultaten').append(errormsg);
-        $('.loading').hide();
-      },
-      timeout: 5000 // set timeout to 5 seconds
-  }); // ajax 1 end
-
-
-function moviemeterCall(id){
+function moviemeterCall(id, mm){
 	$.ajax({
-		    dataType: "jsonp",
-		    url: 'http://www.moviemeter.nl/api/film/' + id + '&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
-		    type: "GET",
-		    cache: false,
-		    async: true,
-		    success: function(details){
-		    	omdbCall(details.imdb);
-		    }
-		   });
+	    dataType: "jsonp",
+	    url: 'http://www.moviemeter.nl/api/film/' + id + '&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
+	    type: "GET",
+	    cache: false,
+	    async: true,
+	    success: function(details){
+	    	omdbCall(details.imdb, mm);
+	    }
+	});
 }
 
-function omdbCall(id){
+function omdbCall(id, mm){
 	$.ajax({
-		    		dataType: "jsonp",
-				    url: 'http://www.omdbapi.com/?apikey=acb7db4&i=' + id,
-				    type: "GET",
-				    cache: false,
-				    async: true,
-				    success: function(data){
+		dataType: "jsonp",
+	    url: 'http://www.omdbapi.com/?apikey=acb7db4&i=' + id,
+	    type: "GET",
+	    cache: false,
+	    async: true,
+	    success: function(omdb){
 
-				    	// $('#movie-id-'+ id).append("<img src='" + omdb.Poster + "' />");
+	    	if( omdb.Poster == undefined || omdb.Poster == "N/A"){
+	    		var poster = "assets/img/no-poster.jpg";
+	    		$('.loading').hide();
+	    	} else {
+	    		var poster = omdb.Poster;
+	    	}
 
-						// document.getElementById("movie-id-82240").setAttribute("src", omdb.Poster);
-
-						console.log(data);
-
-						$('.zoekresultaten').append("<div class='movie col-md-4' id='movie-id-" + data.id + "'><span><a href='film?movie-id=" + data.id + "'>"+ data.title+" (" + data.year + ") </a> <br><img src='assets/img/loading.gif' alt='Film poster'></span>");
-
-						$.each(data, function( key, value ) {
-							console.log(key+value)
-								// $('.apiData-'+index).html(value);
-								// $('body').prepend('<p>'+index+' => '+value+'</p>');
-								// console.log( $('.apiData-'+index).html(value) );
-
-								// 
-						});
-					}
-
-					
-		    	});
+			$('.zoekresultaten').append("<div class='movie col-sm-2 col-md-2 col-lg-2' id='movie-id-" + mm.id + "'><a href='film?movie-id=" + mm.id + "'><p class='film-titel'>"+ mm.title+" (" + mm.year + ") </p></a> <br><a href='film?movie-id=" + mm.id + "'><img src='" + poster + "' alt='Oops! Er ging iets mis met het laden van de filmposter...' style='width: 100%' class='film-poster'/></a>");
+		}
+	});
 }
-
-
-
 </script>
