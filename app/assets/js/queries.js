95e6d32a06521e3ee3aa1f307dde10c37eaf0fbe
mikeo26 <mikeo26@gmail.com>
2016-06-06 15:13:50 +0200

    Films pazjiena

--- app/assets/js/queries.js
+++ app/assets/js/queries.js
@@ -19,3 +19,72 @@ for(i=0; i < 10; i++){
       timeout: 5000 // set timeout to 5 seconds
   });
 }
+
+for(i=0; i < 4; i++){
+  var rand1 =  Math.floor((Math.random() * 100000) + 1);
+  $.ajax({
+    dataType: "jsonp",
+    url: 'http://www.moviemeter.nl/api/film/'+ rand1 +'&api_key=3d91abb206c948bb7ca0b1b9b57be4aa',
+    type: "GET",
+    cache: false,
+    async: true,
+    success: function(data){          
+            $.ajax({
+      dataType: "jsonp",
+        url: 'http://www.omdbapi.com/?apikey=acb7db4&i=' + data.imdb,
+        type: "GET",
+        cache: false,
+        async: true,
+        success: function(omdb){
+
+          if( omdb.Poster == undefined || omdb.Poster == "N/A"){
+            var poster = "assets/img/no-poster.jpg";
+          } else {
+            var poster = omdb.Poster;
+          }
+
+        var title = data.title;
+        var title = title.slice(0, 40);
+
+          filmdata = {
+            id: data.id,
+            title: title,
+            year: data.year,
+            poster: poster,
+            actors: data.actors
+          }
+
+
+
+        var test = filmdata.actors;
+        // if( test.search('leo')){
+        //  console.log('test');
+        // }
+
+        // console.log(filmdata);
+
+
+        if(data.duration == null) {
+          duration = "Speelduur niet beschikbaar";
+        }else {
+          duration = data.duration + " minuten";
+        }
+
+        if(data.average == 0) {
+          var avrating = "Rating niet beschikbaar";
+        }else {
+          var avrating = data.average;
+        }
+
+        $('#filmOverzicht').append("<div class='films' id='movie-id-" + filmdata.id + "'><img src='" + filmdata.poster + "' alt='Oops! Er ging iets mis met het laden van de filmposter...' style='width: 100%' class='film-poster'/><a href='film?movie-id=" + filmdata.id + "'><h4 class='film-titel'>"+ filmdata.title +" (" + filmdata.year + ") </h4></a> <br><a href='film?movie-id=" + filmdata.id + "'></a><span class='filmsInfo'>Rating: " + avrating + " / 5</span><br><span class='filmsInfo'>Speelduur: " + duration + "</span>");
+
+      }
+    });
+      },
+    error: function(){
+        $('#aanbevolenFilmsContent').append(errormsg);
+        $('.loading').hide();
+      },
+      timeout: 5000 // set timeout to 5 seconds
+  });
+}
---
+++ app/films.php
@@ -0,0 +1,12 @@
+<?php include 'head.php'; ?>
+	
+	<div class="container">
+		<div class="row">
+
+		</div>
+				<h1>Films</h1>
+				<div id="filmOverzicht" class="row"></div>
+
+	</div>
+
+<?php include 'footer.php'; ?>
