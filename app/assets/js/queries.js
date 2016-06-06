2fdde694f5c528c4f4ef8b19cc8aa42ccdb7eecf
Menno Laptop <menno.vd.gmail.com>
2016-06-06 15:56:49 +0200

    Films gefixd (col-md-6)

--- app/assets/js/queries.js
+++ app/assets/js/queries.js
@@ -1,5 +1,6 @@
 var errormsg = '<p class="errormsg reload" onClick="window.location.reload()">De content kon niet worden geladen <br /> Klik hier om het nog een keer te proberen.</p>';
 
+// Home pagina - Aanbevolen films
 for(i=0; i < 10; i++){
   var rand =  Math.floor((Math.random() * 100000) + 1);
   $.ajax({
@@ -20,6 +21,13 @@ for(i=0; i < 10; i++){
   });
 }
 
+
+/*********************************************************************************************************************************/
+/*********************************************************************************************************************************/
+/*********************************************************************************************************************************/
+
+
+// Films pagina
 for(i=0; i < 4; i++){
   var rand1 =  Math.floor((Math.random() * 100000) + 1);
   $.ajax({
@@ -28,9 +36,10 @@ for(i=0; i < 4; i++){
     type: "GET",
     cache: false,
     async: true,
-    success: function(data){          
-            $.ajax({
-      dataType: "jsonp",
+    success: function(data){
+
+      $.ajax({
+        dataType: "jsonp",
         url: 'http://www.omdbapi.com/?apikey=acb7db4&i=' + data.imdb,
         type: "GET",
         cache: false,
@@ -43,8 +52,8 @@ for(i=0; i < 4; i++){
             var poster = omdb.Poster;
           }
 
-        var title = data.title;
-        var title = title.slice(0, 40);
+          var title = data.title;
+          var title = title.slice(0, 40);
 
           filmdata = {
             id: data.id,
@@ -54,29 +63,21 @@ for(i=0; i < 4; i++){
             actors: data.actors
           }
 
+          var test = filmdata.actors;
 
+          if(data.duration == null) {
+            duration = "Speelduur niet beschikbaar";
+          }else {
+            duration = data.duration + " minuten";
+          }
 
-        var test = filmdata.actors;
-        // if( test.search('leo')){
-        //  console.log('test');
-        // }
+          if(data.average == 0) {
+            var avrating = "Rating niet beschikbaar";
+          }else {
+            var avrating = data.average;
+          }
 
-        // console.log(filmdata);
-
-
-        if(data.duration == null) {
-          duration = "Speelduur niet beschikbaar";
-        }else {
-          duration = data.duration + " minuten";
-        }
-
-        if(data.average == 0) {
-          var avrating = "Rating niet beschikbaar";
-        }else {
-          var avrating = data.average;
-        }
-
-        $('#filmOverzicht').append("<div class='films' id='movie-id-" + filmdata.id + "'><img src='" + filmdata.poster + "' alt='Oops! Er ging iets mis met het laden van de filmposter...' style='width: 100%' class='film-poster'/><a href='film?movie-id=" + filmdata.id + "'><h4 class='film-titel'>"+ filmdata.title +" (" + filmdata.year + ") </h4></a> <br><a href='film?movie-id=" + filmdata.id + "'></a><span class='filmsInfo'>Rating: " + avrating + " / 5</span><br><span class='filmsInfo'>Speelduur: " + duration + "</span>");
+          $('#filmOverzicht').append("<div class='films col-md-6' id='movie-id-" + filmdata.id + "'><img src='" + filmdata.poster + "' alt='Oops! Er ging iets mis met het laden van de filmposter...' style='width: 100%' class='film-poster'/><a href='film?movie-id=" + filmdata.id + "'><h4 class='film-titel'>"+ filmdata.title +" (" + filmdata.year + ") </h4></a> <br><a href='film?movie-id=" + filmdata.id + "'></a><span class='filmsInfo'>Rating: " + avrating + " / 5</span><br><span class='filmsInfo'>Speelduur: " + duration + "</span>");
 
       }
     });
