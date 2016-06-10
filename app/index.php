674a92615346ae0e9a4063cbc303293fe38203b4
mikeo26 <mikeo26@gmail.com>
2016-06-10 11:25:25 +0200

    Planning optimalisatie en eerste punt verwrekt

--- app/index.php
+++ app/index.php
@@ -16,6 +16,9 @@
 			<div class="col-sm-8 col-md-8 col-lg-8">
 				<h2>Movie Database Web App</h2>
 				<p>Op deze website kun je je eigen gebruikersaccount aanmaken om je persoonlijke films collectie, bekeken, watchlist en wishlist bij te houden.</p>
+				<p>Als je in bent gelogd kun je eenvoudig navigeren naar een film waarvan jij meer informatie wilt zien. Op de homepage en film pagina staan een aantal willekeurige suggesties die je wellicht leuke inspiratie geven bij het zoeken van een geschikte film.</p>
+				<p>In het geval dat je al een bepaald trefwoord, jaartal of genre in gedachten hebt kun je hiervoor gebruik van de (geavanceerde) zoekfunctie. Als je dan een film naar wens hebt gevonden kun je deze aan je eigen wishlist, watchlist, bekeken lijst of collectie toevoegen. Zo heb je een mooi overzicht van je eigen verzameling.</p>
+				<p>Mochten er nog zaken onduidelijk zijn, neem dan gerust contact met ons op. We zullen uw vraag zo spoedig mogelijk beantwoorden.</p>
 			</div>
 
 			<div class="col-sm-4 col-md-4 col-lg-4">
